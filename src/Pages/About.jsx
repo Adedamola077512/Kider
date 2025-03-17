bfaaae9cf8301d17ec706d49034bbcaf75ca070e
Adedamola <adedamolaobadimu@gmail.com>
2025-03-17 09:02:11 +0100

    css update

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import LearnMore from '../components/LearnMore';
 import BecomeATeacher from '../components/BecomeATeacher';
-// import PopularTeachers from '../components/PopularTeachers';
 const About = () => {
   return (
     <div>
       <h1 className='Facilities-h1 text-center mt-5'><b>About Us</b></h1>
         <LearnMore />
         <BecomeATeacher />
-        {/* <PopularTeachers /> */}
     </div>
   )
 }
